572bddedcc2fdf59c201652920f439181c98fd4d
shalevgerbi <57968427+shalevgerbi@users.noreply.github.com>
2021-08-30 18:01:12 +0300

    app build

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Avatar from '../components/Avatar'
-import {MicrophoneIcon,ViewGridIcon} from "@heroIcons/react/solid"
+import {MicrophoneIcon,ViewGridIcon} from "@heroicons/react/solid"
 import Image from "next/image"
 import { SearchIcon } from '@heroicons/react/outline'
 import Footer from '../components/Footer'
