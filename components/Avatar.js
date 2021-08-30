697706b3d354452335dc01ba0a8442cd20cf1ae9
shalevgerbi <57968427+shalevgerbi@users.noreply.github.com>
2021-08-30 17:36:20 +0300

    app build

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,7 +3,7 @@ function Avatar({url,className}) {
         <img className={`rounded-full h-10 w-10 cursor-pointer transition duration-150 transform hover:scale-110 ${className}`} 
         loading="lazy"
         src={url}
-        alt="profile pcture"
+        alt="profile picture"
         />
             
         
